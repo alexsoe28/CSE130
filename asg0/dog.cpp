86a5df7d557b33e9c3118c85ccf63a1c1e3249e5
asoe <asoe@ucsc.edu>
2019-10-03 16:37:07 -0700

    Can now print really big files as well as multiple files

--- asg0/dog.cpp
+++ asg0/dog.cpp
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <iostream>
 
 #include <sys/types.h>
 #include <sys/stat.h>
@@ -8,29 +9,23 @@
 #include <unistd.h>
 int main(int argc, char *argv[])
 {
-    size_t bytes;
+    size_t bytes = 1;
     char buff[32768];
-    size_t fileDescriptor; 
-    for (int i = 1; i <= argc; i++)
+    int fileDescriptor; 
+    for (int i = 1; i < argc; i++)
     {
         fileDescriptor = open(argv[i], O_RDONLY);
         if(fileDescriptor < 0){
-            fprintf(stderr, "dog: %s : No such file or directory", argv[1]);
+            fprintf(stdout, "dog: %s : No such file or directory\n", argv[i]);
         }
         else{
-            bytes = read(fileDescriptor, buff, sizeof(buff) - 1);
-            write(1, buff, bytes);
+            while(bytes != 0){
+                bytes = read(fileDescriptor, buff, sizeof(buff) - 1);
+                write(1, buff, bytes);
+            }
+            bytes = 1;
         }
         close(fileDescriptor); 
     }
-    /*size_t fileDescriptor = open(argv[1], O_RDONLY);
-    if(fileDescriptor < 0){
-        fprintf(stderr, "dog: %s : No such file or directory", argv[1]);
-    }
-    else{
-        bytes = read(fileDescriptor, buff, sizeof(buff) - 1);
-        write(1, buff, bytes);
-    }
-    */    
     return 0;        
 }
