5db4797de303e8ceca6d3d6782912d9bc9d44c11
asoe <asoe@ucsc.edu>
2019-10-03 00:55:51 -0700

    Can now print multiple files

--- asg0/dog.cpp
+++ asg0/dog.cpp
@@ -9,15 +9,28 @@
 int main(int argc, char *argv[])
 {
     size_t bytes;
-    char buff[32];
-    size_t fileDescriptor = open(argv[2], O_RDONLY);
+    char buff[32768];
+    size_t fileDescriptor; 
+    for (int i = 1; i <= argc; i++)
+    {
+        fileDescriptor = open(argv[i], O_RDONLY);
+        if(fileDescriptor < 0){
+            fprintf(stderr, "dog: %s : No such file or directory", argv[1]);
+        }
+        else{
+            bytes = read(fileDescriptor, buff, sizeof(buff) - 1);
+            write(1, buff, bytes);
+        }
+        close(fileDescriptor); 
+    }
+    /*size_t fileDescriptor = open(argv[1], O_RDONLY);
     if(fileDescriptor < 0){
-        fprintf(stderr, "dog: %s : No such file or directory", argv[2]);
+        fprintf(stderr, "dog: %s : No such file or directory", argv[1]);
     }
     else{
         bytes = read(fileDescriptor, buff, sizeof(buff) - 1);
         write(1, buff, bytes);
-    }    
-    close(fileDescriptor); 
+    }
+    */    
     return 0;        
 }
