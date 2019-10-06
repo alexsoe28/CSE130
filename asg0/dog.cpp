fe40e7c763f97154e02b2af8bbea73a9e89c4ae9
asoe <asoe@ucsc.edu>
2019-10-06 01:41:40 -0700

    Can now check if a label is a file

--- asg0/dog.cpp
+++ asg0/dog.cpp
@@ -15,23 +15,24 @@ int main(int argc, char *argv[])
     
     for (int i = 1; i < argc; i++)
     {
+        fileDescriptor = open(argv[i], O_RDONLY);
         if(strncmp(argv[i],"-", 1) == 0)
         {
             while(true)
             {
-                char buffer [32768] = "";
-                read(0, buffer, sizeof(buff) - 1);
-                write(1, buffer, sizeof(buff) - 1);
+                memset(buff, 0, sizeof buff);
+                read(0, buff, sizeof(buff) - 1);
+                write(1, buff, sizeof(buff) - 1);
             }
+        }
+        else if(fileDescriptor < 0 && strncmp(argv[i],"-", 1) != 0)
+        {
+            fprintf(stdout, "dog: %s : No such file or directory\n", argv[i]);
         }
         else
         {
-            if(fileDescriptor < 0 && strncmp(argv[i],"-", 1) != 0)
-            {
-                fprintf(stdout, "dog: %s : No such file or directory\n", argv[i]);
-            }
-            fileDescriptor = open(argv[i], O_RDONLY);
             while(bytes != 0){
+                memset(buff, 0, sizeof buff);
                 bytes = read(fileDescriptor, buff, sizeof(buff) - 1);
                 write(1, buff, bytes);
             }
