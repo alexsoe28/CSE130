7f12f1abfd14bd90d0cc306dac311dcf94ceec5a
Alexander Soe <asoe@ucsc.edu>
2019-10-10 00:49:23 -0700

    Can now detect if a file is adirectory

--- asg0/dog.cpp
+++ asg0/dog.cpp
@@ -10,8 +10,8 @@ int main(int argc, char *argv[])
 {
     int fileDescriptor;
     char buff[32768] = "";
-    size_t bytes = 1;
-    size_t endOfFile = 1;
+    int bytes = 1;
+    int endOfFile = 1;
     int fileNumber = 1;
     //Changes fileNumber only if ./dog is typed
     if(argc == 1)
@@ -21,6 +21,7 @@ int main(int argc, char *argv[])
     while(argv[fileNumber] != NULL)
     {
         fileDescriptor = open(argv[fileNumber], O_RDONLY);
+		bytes = read(fileDescriptor, buff, sizeof(buff)); 
         //Checks the stdin for the key char "-" and then prints user input to stdout   
 		if(strncmp(argv[fileNumber],"-", 1) == 0 || argc == 1)
         {
@@ -39,24 +40,25 @@ int main(int argc, char *argv[])
                 }
             }
         }
-        //If the the name is not a file and is not "-" print an error
-        else if(fileDescriptor < 0 && strncmp(argv[fileNumber],"-", 1) != 0)
+        //If the the name is not a file, is a directory, or is not "-" print an error
+  		else if((fileDescriptor < 0 && strncmp(argv[fileNumber],"-", 1) != 0) || bytes < 0)
         {
            warn("%s", argv[fileNumber]); 
         }
         //If the file does exist print out the contents.
         else
         {
+			write(1, buff, bytes);
             while(bytes != 0)
             {
                 memset(buff, 0, sizeof buff);
-                bytes = read(fileDescriptor, buff, sizeof(buff) - 1);
+        		bytes = read(fileDescriptor, buff, sizeof(buff));
                 write(1, buff, bytes);
             }
             bytes = 1;
             close(fileDescriptor); 
         }
-        fileNumber++;	
+        fileNumber++;
     }
     return 0;        
 }
