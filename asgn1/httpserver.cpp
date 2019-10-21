3ca69718165a67c312d90abd2b2ec704dc526654
asoe <asoe@ucsc.edu>
2019-10-21 02:17:24 -0700

    Added user port and address compatibility

--- asgn1/httpserver.cpp
+++ asgn1/httpserver.cpp
@@ -4,41 +4,71 @@
 #include <stdlib.h>
 #include <netinet/in.h>
 #include <string.h>
+#include <string>
+#include <arpa/inet.h>
 #define PORT 8080
 
-/*
-void handle_client(int fd)
-{
-    char response_data[2048];
-    char data[2048] = "HTTP/1.1 200 OK\r\n";
-    
-}
-*/
-int main()
+int main(int argc, char *argv[])
 {
     int server_fd, client_socket;
     struct sockaddr_in address;
     int addrlen = sizeof(address);
-    char buffer[1024] = {0};
+    int const buffSize = 4097;
+    char buffer[buffSize];
+
+    //Check if the user inputs too many arguments
+    if(argc > 2)
+    {
+        perror("ERROR: Invalid arguments");
+        exit(EXIT_FAILURE);
+    }
+
+    //Check if the socket is setup succesfully
     if((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0)
     {
         perror("ERROR: Socket failed");
         exit(EXIT_FAILURE);
     }
+
+    //Initialize port and address
     address.sin_family = AF_INET;
     address.sin_addr.s_addr = INADDR_ANY;
-    address.sin_port = htons(PORT);
-    
+    int user_port;
+    if(argc < 1)
+    {
+        address.sin_port = htons(PORT);
+    }
+    else if (argc < 2)
+    {
+        address.sin_port = htons(PORT);
+        address.sin_addr.s_addr = inet_addr(argv[1]);   
+    }
+    else if(sscanf(argv[2], "%d", &user_port) != -1)
+    {
+        address.sin_port = htons(user_port);
+        address.sin_addr.s_addr = inet_addr(argv[1]);       
+    } 
+    else
+    {
+        perror("ERROR: Invalid ");
+        exit(EXIT_FAILURE);
+    }
+
+    //Bind address to server
     if(bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0)
     {
         perror("ERROR: Bind failed");
         exit(EXIT_FAILURE);
     }
+
+    //Listen for a client socket
     if(listen(server_fd, 3) < 0)
     {
         perror("ERROR: Listen failed");
         exit(EXIT_FAILURE);
     }
+
+    //Respond to get and put requests
     while(1)
     {
         if((client_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t*)&addrlen)) < 0)
@@ -52,9 +82,6 @@ int main()
             perror("ERROR: Could not read from socket");
             exit(EXIT_FAILURE);
         }
-        printf("%s\n", buffer);
-        client_data = write(client_socket, "I got your message", 18);
-        //handle_client(client_socket);
         close(client_socket);
     }
     
