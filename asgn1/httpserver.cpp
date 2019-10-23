9d18be4382f008950ca44f0559e83ab0fac9d054
asoe <asoe@ucsc.edu>
2019-10-23 00:08:59 -0700

    Can read header and print out the file output to stdout

--- asgn1/httpserver.cpp
+++ asgn1/httpserver.cpp
@@ -6,15 +6,56 @@
 #include <string.h>
 #include <string>
 #include <arpa/inet.h>
+#include <iostream>
 #define PORT 8080
+
+const size_t buffSize = 4097;
+
+std::string readHeader(int fd)
+{
+    char buffer[buffSize];
+    char *p;
+    size_t len = buffSize - 1;
+    ssize_t n;
+    p = buffer;
+
+    while((n = read(fd, p, len)) > 0)
+    {
+        len = len - n;
+        p = p + n;
+        if(strstr(buffer, "\n\r") != nullptr)
+        {
+            printf("%s\n", "We have reached the end of the header");
+            break;
+        }
+        if(len == 0)
+        {
+            break;
+        }
+    }
+    *p = '\0';
+    if(n < 0)
+    {
+        perror("ERROR: Can't read Client Header");
+        exit(EXIT_FAILURE);
+    }
+    if (len == 0)
+    {
+        perror("ERROR: Exceeded max header size");
+        exit(EXIT_FAILURE);
+    }
+    return std::string(buffer);
+}
 
 int main(int argc, char *argv[])
 {
-    int server_fd, client_socket;
+    char buff[buffSize];
+    char const http_header[2048] = "HTTP/1.1 200 OK\r\n";
+    int server_fd, client_fd;
     struct sockaddr_in address;
     int addrlen = sizeof(address);
-    int const buffSize = 4097;
-    char buffer[buffSize];
+    //int const buffSize = 4097;
+    //char buffer[buffSize];
 
     //Check if the user inputs too many arguments
     if(argc > 2)
@@ -34,11 +75,11 @@ int main(int argc, char *argv[])
     address.sin_family = AF_INET;
     address.sin_addr.s_addr = INADDR_ANY;
     int user_port;
-    if(argc < 1)
+    if(argc == 1)
     {
         address.sin_port = htons(PORT);
     }
-    else if (argc < 2)
+    else if (argc == 2)
     {
         address.sin_port = htons(PORT);
         address.sin_addr.s_addr = inet_addr(argv[1]);   
@@ -71,18 +112,18 @@ int main(int argc, char *argv[])
     //Respond to get and put requests
     while(1)
     {
-        if((client_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t*)&addrlen)) < 0)
+        if((client_fd = accept(server_fd, (struct sockaddr *)&address, (socklen_t*)&addrlen)) < 0)
         {
             perror("ERROR: Could not accept socket");
             exit(EXIT_FAILURE);
         }
-        int client_data = read(client_socket, buffer, sizeof buffer);
-        if(client_data < 0)
-        {
-            perror("ERROR: Could not read from socket");
-            exit(EXIT_FAILURE);
-        }
-        close(client_socket);
+        write(client_fd, http_header, sizeof http_header);
+        std::string header = readHeader(client_fd); 
+        printf("%s\n", header.c_str());
+        std::cout << header.length() << std::endl;
+        read(client_fd, buff, sizeof buff - 1);
+        write(1, buff, sizeof buff - 1);
+        close(client_fd);
     }
     
     close(server_fd);
